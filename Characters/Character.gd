58aef83999f8d89fbc273898d2dc3d86b6ebc942
Scrafi4 <122440587+Scrafi4@users.noreply.github.com>
2023-02-08 22:16:36 +0300

    Initial commit

---
+++ Characters/Character.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+class_name Character
+
+export var speed: = Vector2(300.0, 1000.0)
+export var gravity: = 4000.0
+
+
+const FLOOR_NORMAL: = Vector2.UP
+
+var _velocity: = Vector2.ZERO
+
+
+	
+	
